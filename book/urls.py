6f395c8c3715d68eba853d1c43e0297c62178a0d
syifakaffa <syifa.kaffa@ui.ac.id>
2023-10-11 17:14:27 +0700

    update databse

--- book/urls.py
+++ book/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
+from .views import get_json
 from main.views import show_main
-from .api import BookListView
-
-
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('api/books/', BookListView.as_view(), name='book-list'),
+    path('api/books/', get_json, name='book-list'),
     
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+
+from .models import Book
 
 # Create your views here.
+
+def get_json(self):
+    data = Book.objects.filter(pk=1)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
