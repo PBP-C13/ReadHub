da0d8ff9bca8d725df3672258d2c745780f24ba0
Aryan Primasatya <aryanstya238@gmail.com>
2023-10-22 11:11:21 +0700

    Register

--- main/views.py
+++ main/views.py
@@ -7,12 +7,13 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
+from django.contrib import messages
+from book.models import Book
 
 # Create your views here.
 
 def show_main(request):
-    products = Product.objects.all()
+    products = Book.objects.all()
 
     context = {
         'name': 'Pak Bepe', # Nama kamu
@@ -38,3 +39,15 @@ def show_json(request):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
