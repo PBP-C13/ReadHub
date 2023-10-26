4e13b6f6c1acd62d76ffcef62507f2cb3365f064
lontonggg <reyhan.zada@ui.ac.id>
2023-10-27 01:58:09 +0700

    Bikin models, forms, templates, dan bikin method di views buat minjem

--- borrow_flow/models.py
+++ borrow_flow/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from book.models import Book
+from datetime import datetime, timedelta
 
 # Create your models here.
+class BorrowedBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_duration = models.IntegerField()
+    return_date = models.DateTimeField(default=datetime.now() + timedelta(days=borrow_duration))
+    terms_accepted = models.BooleanField(default=False)
+
+
+    
--- borrow_flow/views.py
+++ borrow_flow/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from borrow_flow.models import Book, BorrowedBook
+from borrow_flow.forms import *
 
 # Create your views here.
+def show_borrow_page(request, id):
+    book = Book.objects.get(pk = id)
+    context = {
+        'user' : request.user.username,
+        'book' : book
+    }
+    return render(request, "borrow.html", context)
+
+def borrow_book(request, id):
+    book = Book.objects.get(pk=id)
+    if request.method == 'POST':
+        form = BorrowForm(request.POST)
+        if form.is_valid() and form.cleaned_data['terms_accepted']:
+            borrowed_book = form.save(commit=False)
+            borrowed_book.book = book
+            borrowed_book.user = request.user
+            borrowed_book.save()
+            return redirect()
+    
+    context = {
+        'form' : form, 
+        'book' : book
+    }
+    return render(request, 'borrow.html', context)
+
+def return_book(request, id):
+    return
+
