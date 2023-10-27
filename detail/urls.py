caa36fb037c97749d1ca6d141679a28cefff5a77
Aiza <aizaderisyana@yahoo.co.id>
2023-10-28 01:28:44 +0800

    try

--- detail/urls.py
+++ detail/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import  *
+from detail.views import show_detail, borrow_flow, get_item_json, create_review
 
 app_name = 'detail'
 
 urlpatterns = [
-    path('detail/<int:id>/', show_detail, name='show_detail')
+    path('', show_detail, name='show_detail'),
+    path('borrow_flow/', borrow_flow, name='borrow_flow'),
+    path('get-item-json/', get_item_json, name='get_item_json'),
+    path('create-review/', create_review, name='create_review'),
+    
 ]
