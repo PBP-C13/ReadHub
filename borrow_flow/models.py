a766e8240fc685c196101c6ec486e657fd5ec3f8
lontonggg <reyhan.zada@ui.ac.id>
2023-10-28 17:15:08 +0700

    Menambahkan fungsi mengembalikan buku

--- borrow_flow/models.py
+++ borrow_flow/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from book.models import Book
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class BorrowedBook(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    borrow_duration = models.IntegerField()
-    return_date = models.DateTimeField()
-    terms_accepted = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, name='user')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, name='book')
+    borrow_duration = models.IntegerField(name='borrow_duration', validators=[MinValueValidator(0)])
+    borrow_date = models.DateField(auto_now_add=True, name='borrow_date')
+    return_date = models.DateField(name='return_date')
+    terms_accepted = models.BooleanField(default=False, name='terms_accepted')
 
 
     
