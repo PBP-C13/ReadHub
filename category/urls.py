e098565cc46e0a520dd3e3ed386b0bea7aac3064
syifakaffa <syifa.kaffa@ui.ac.id>
2023-10-29 01:29:15 +0700

    delete favorit

--- category/urls.py
+++ category/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from category.views import show_category, get_product_json, add_book_favorit_ajax, show_favorit, get_book_favorit
+from category.views import show_category, get_product_json, add_book_favorit_ajax, show_favorit, get_book_favorit, delete_favorit
 from detail.views import show_detail
 
 app_name = 'category'
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', show_category, name='show_category'),
     path('detail/<int:id>/', show_detail, name='show_detail'),
     path('add_favorit/<int:book_id>/', add_book_favorit_ajax, name='book-list'),
+    path('delete_favorit/<int:book_id>/', delete_favorit, name='delete_book'),   
     path('show-favorit', show_favorit, name='show_favorit'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('get-book-favorit/', get_book_favorit, name='get_book_favorit'),
