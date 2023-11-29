cbc8230b868d508af2e90828fe232b5973715bde
aryawijayak <aryawijayak2004@gmail.com>
2023-11-29 21:22:46 +0700

    auth

---
+++ authentication/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Username'
+        self.fields['password1'].label = 'Password'
+        self.fields['password2'].label = 'Confirm Password'
