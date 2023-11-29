5a2d7d635b41506e74205cb4c5acb7017bac6776
aryawijayak <aryawijayak2004@gmail.com>
2023-11-29 21:01:31 +0700

    crsf

--- ReadHub/settings.py
+++ ReadHub/settings.py
@@ -156,3 +156,6 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://readhub-c13-tk.pbp.cs.ui.ac.id/']
