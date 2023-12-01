a8b6a509fc82253918023f91595e4c4e6ea05353
Aiza <aizaderisyana@yahoo.co.id>
2023-12-01 12:15:10 +0700

    correct

--- detail/urls.py
+++ detail/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from detail.views import show_detail, get_item_json, create_review
+from detail.views import show_detail, get_item_json, create_review_ajax
 from borrow_flow.views import show_borrow_page
 
 app_name = 'detail'
 
 urlpatterns = [
-    path('get-item-json/', get_item_json, name='get_item_json'),
-    path('create_review/', create_review, name='create_review'),  
-    path('detail/<int:id>/', show_detail, name='show_detail'),  
+    path('<int:id>/get-item-json/', get_item_json, name='get_item_json'),
+    path('create_review_ajax/', create_review_ajax, name='create_review_ajax'),  
+    path('<int:id>/', show_detail, name='show_detail'),  
     path('<int:id>/', show_borrow_page, name='show_borrow_page'),
 ]
--- detail/views.py
+++ detail/views.py
@@ -1,50 +1,61 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.core import serializers
+from django.shortcuts import render
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from detail.models import Detail, Review
+from detail.forms import ReviewForm
 from book.models import Book
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 import random
+from detail.models import Review
 
 def show_detail(request, id):
-    detail = Book.objects.get(pk=id)
     selected_book = Book.objects.get(pk=id)
     genres = selected_book.genres.split('|')[:3]
     selected_genres = random.sample(genres, 3)
 
     similar_books = Book.objects.filter(
-    Q(genres__icontains=selected_genres[0]) | Q(genres__icontains=selected_genres[1]) | Q(genres__icontains=selected_genres[2])
+        Q(genres__icontains=selected_genres[0]) | Q(genres__icontains=selected_genres[1]) | Q(genres__icontains=selected_genres[2])
     ).exclude(pk=id)[:6]
 
+    book_reviews = Review.objects.filter(book=selected_book)
+
     context = {
-        'detail': detail,
         'book': selected_book,
-        'name': request.user.username if request.user.is_authenticated else "Guest",
+        'user': request.user.username if request.user.is_authenticated else "Guest",
         'similar_books': similar_books,
         'genres': genres,
-        'selected_genres' : selected_genres
-        
+        'selected_genres': selected_genres,
+        'reviews': book_reviews  # Add this line
     }
     return render(request, "detail.html", context)
 
-def get_item_json(request):
-    all_books = Book.objects.all()
-    return HttpResponse(serializers.serialize('json', all_books))
 
+def get_item_json(request, id):
+    product_items = []
+    # Filter reviews by book id
+    reviews = Review.objects.filter(book_id=id)
+    
+    for r in reviews:
+        product_item = {
+            'user': r.user.username if r.user else "Guest",
+            'review': r.review
+        }
+        product_items.append(product_item)
+
+    return JsonResponse(product_items, safe=False)
+    
 @login_required
 @csrf_exempt
-def create_review(request):
-    if request.method == "POST":
-        review = request.POST.get("book_review")
-        name = request.POST.get("name")
-
-        # Create a new review associated with the book
-        newReview = Review(name=name, review=review)
+def create_review_ajax(request):
+    if request.method == 'POST':
+        review_text = request.POST.get("book_review")
+        book_id = request.POST.get("book_id")  
+        book = Book.objects.get(pk=book_id)
+        newReview = Review(user=request.user, review=review_text, book=book)
         newReview.save()
-
-        return HttpResponse(b"CREATED", status=201)
-    return HttpResponseNotFound()
+        
+    return HttpResponse(b"CREATED", status=201)
