4adb22a74d3237ae013509cb479fc6991569bc95
Aiza <aizaderisyana@yahoo.co.id>
2023-12-04 15:43:48 +0700

    correct error

--- detail/models.py
+++ detail/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User  
+from book.models import Book
+
 
 class Detail(models.Model):
     similar_books =  models.TextField(null=True, blank=True)
     
 class Review(models.Model):
-    review = models.ForeignKey(Detail, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)  # Link to a Book
+    review = models.TextField()
--- detail/urls.py
+++ detail/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from detail.views import show_detail, get_item_json, create_review
+from detail.views import show_detail, get_item_json, create_review_ajax, create_review
 from borrow_flow.views import show_borrow_page
 
 app_name = 'detail'
 
 urlpatterns = [
-    path('get-item-json/', get_item_json, name='get_item_json'),
+    path('<int:id>/get-item-json/', get_item_json, name='get_item_json'),
+    path('create_review_ajax/', create_review_ajax, name='create_review_ajax'),  
     path('create_review/', create_review, name='create_review'),  
-    path('detail/<int:id>/', show_detail, name='show_detail'),  
+    path('<int:id>/', show_detail, name='show_detail'),  
     path('<int:id>/', show_borrow_page, name='show_borrow_page'),
 ]
