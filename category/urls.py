869fd604eece429bda1a85259403ef7063e7409b
Aiza <aizaderisyana@yahoo.co.id>
2023-12-10 22:00:34 +0700

    add new detail code for flutter

--- category/urls.py
+++ category/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from category.views import show_category, get_product_json, add_book_favorit_ajax, show_favorit, get_book_favorit, delete_favorit, show_json_favorit
-from detail.views import show_detail
+from detail.views import create_review_flutter, get_item_json, get_item_json_flutter, show_detail
 
 app_name = 'category'
 
@@ -13,4 +13,7 @@ urlpatterns = [
     path('get-product/', get_product_json, name='get_product_json'),
     path('get-book-favorit/', get_book_favorit, name='get_book_favorit'),
     path('json/', show_json_favorit, name='show_json'), 
+    path('detail/<int:id>/get-item-json/', get_item_json, name='get_item_json'),
+    path('detail/<int:id>/create_review_flutter', create_review_flutter, name='create_review_flutter'),
+    path('detail/<int:id>/get-item-json-flutter/', get_item_json_flutter, name='get_item_json_flutter'),
 ]
